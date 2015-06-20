c92f1663737a9b23e37417187f187fbed30f4694
Duy K. Bui <duy@buifamily.info>
2015-06-20 13:19:07 -0400

    Add install script for Ubuntu 14.04 and update README

---
+++ install/setup.sh
@@ -0,0 +1,13 @@
+sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo apt-add-repository -y 'deb [arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main'
+sudo apt-get update
+sudo apt-get install -y mongodb dart python python-pip python-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev
+sudo ln -s /usr/lib/dart/bin/pub /usr/bin
+cd ~/quickpin
+sudo pip install virtualenv
+virtualenv/venv
+source venv/bin/activate
+pip install -r install/python-dependencies.txt
+cd ~/quickpin/static/dart
+pub get
+pub build
